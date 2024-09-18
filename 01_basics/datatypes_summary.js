d2f39c0dc7534bc985a779381aade8cd3d6a44d1
Ranjan Kumar Mahato <126558460+Ranjanrkm@users.noreply.github.com>
2024-09-18 11:32:57 +0000

    datatypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -36,3 +36,30 @@ console.log(typeof bigNumber);
 console.log(typeof outsideTemp);
 
 console.log(typeof myFunction);
+
+
+
+// ***********************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com"
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
